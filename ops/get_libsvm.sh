0e614f3ba1eb7e6db27f253b24249b22c1461150
Breno Lima de Freitas <brenolimadefreitas@gmail.com>
2016-05-22 22:29:19 -0300

    Moves dependencies to folder

---
+++ ops/get_libsvm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ./algs/svm/
+
+wget -O ./_tmp.zip http://www.csie.ntu.edu.tw/~cjlin/cgi-bin/libsvm.cgi?+http://www.csie.ntu.edu.tw/~cjlin/libsvm+zip
+
+unzip -q ./_tmp.zip
+
+rm ./_tmp.zip
+
+cd ./libsvm*
+
+matlab -nodesktop -nosplash -nojvm < ../../../ops/compilesvm.m
