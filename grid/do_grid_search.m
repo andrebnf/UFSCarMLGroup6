a6f30a127fcca359b2aeab1caccbee1be3d421eb
Breno Lima de Freitas <brenolimadefreitas@gmail.com>
2016-05-22 21:52:34 -0300

    Remove comment

--- grid/do_grid_search.m
+++ grid/do_grid_search.m
@@ -11,18 +11,18 @@ function bests = do_grid_search(dfx, losses)
 
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-  % upper_bound = floor(sqrt(size(training, 1)));
-  %
-  % if mod(upper_bound, 2) == 0
-  %   upper_bound = upper_bound - 1;
-  % end
-  %
-  % constants = [primes(17) ceil(upper_bound / 2) upper_bound];
-  %
-  % constants = unique(constants(2 : end));
-  % 
-  % bests.kNN = call_grid('kNN', 'K', ...
-  %  constants, training, training_labels_bool, @apply_knn, @knn_error);
+  upper_bound = floor(sqrt(size(training, 1)));
+
+  if mod(upper_bound, 2) == 0
+    upper_bound = upper_bound - 1;
+  end
+
+  constants = [primes(17) ceil(upper_bound / 2) upper_bound];
+
+  constants = unique(constants(2 : end));
+
+  bests.kNN = call_grid('kNN', 'K', ...
+   constants, training, training_labels_bool, @apply_knn, @knn_error);
 
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
